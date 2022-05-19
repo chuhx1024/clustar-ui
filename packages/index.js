caf0d3681480b0739348a64e469c8b6ac6912d5a
chuhx <chuhx1024@126.com>
2022-05-19 14:48:54 +0800

    feat: packages install

---
+++ packages/index.js
@@ -0,0 +1,25 @@
+import Button from './button/index.js'
+import Icon from './icon/index.js'
+
+const components = [
+    Button,
+    Icon,
+]
+
+const install = (Vue) => {
+    components.forEach(component => {
+        Vue.component(component.name, component)
+    })
+}
+
+// if (typeof window !== 'undefined' && window.Vue) {
+//     install(window.Vue)
+// }
+
+// vue 插件机制  导出一个对象的话  这个对象要有一个 install 方法
+export default {
+    version: '0.0.1',
+    install,
+    Button,
+    Icon,
+}
--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import CluButton from '../packages/button'
-import CluIcon from '../packages/icon'
+// import CluButton from '../packages/button'
+// import CluIcon from '../packages/icon'
+import CluUI from '../packages/index.js'
+// import CluUI from '../dist/clustar-ui.umd.min'
+// import '../dist/clustar-ui.css'
 import '../src/assets/layout.scss'
 Vue.config.productionTip = false
 
-Vue.use(CluButton)
-Vue.use(CluIcon)
+// Vue.use(CluButton)
+// Vue.use(CluIcon)
+Vue.use(CluUI)
 
 new Vue({
     router,
